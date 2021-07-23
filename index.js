2f94b8e6ddd2ffe7ef15d1469b220a6071482ffb
Rudi Rutanen <rudi.rutanen@hotmail.com>
2021-07-23 17:20:13 +0300

    cors and procfile added

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const morgan = require('morgan')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
+const cors = require('cors')
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     { 
@@ -94,7 +97,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
