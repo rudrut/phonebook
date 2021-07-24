c4e3d6f10f36e7aeffd1fdfac8c5492456965187
Rudi Rutanen <rudi.rutanen@hotmail.com>
2021-07-25 00:02:41 +0300

    small change

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
-
   next(error)
 }
 
